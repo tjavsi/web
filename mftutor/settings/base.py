152c7027298e2a41bc07716607a33558509f3ec2
Mathias Rav <rav@cs.au.dk>
2016-04-05 19:14:32 +0200

    Update MIDDLEWARE_CLASSES to Django 1.9 standards

--- mftutor/settings/base.py
+++ mftutor/settings/base.py
@@ -103,13 +103,14 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'mftutor.tutor.middleware.TutorMiddleware',
 )
